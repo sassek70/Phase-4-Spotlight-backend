fcce7999620a509232e8196b3b542b34c1e2f004
Kevin Sasse <sassek70@gmail.com>
2022-12-05 16:59:02 -0500

    added validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ApplicationRecord
+    validates :username, presence: true
+    validates :password, presence: true
+    
     has_many :user_events
     has_many :events, through: :user_events
     has_many :user_venues
--- app/models/user_event.rb
+++ app/models/user_event.rb
@@ -1,4 +1,7 @@
 class UserEvent < ApplicationRecord
+    validates :user_id, presence: true, numericality: {only_integer: true}
+    validates :event_id, presence: true, numericality: {only_integer: true}
+
     belongs_to :user
     belongs_to :event
 end
