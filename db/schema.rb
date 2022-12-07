d0dff4c4af3581c58336e5825ec373500c006c86
Kevin Sasse <sassek70@gmail.com>
2022-12-07 11:27:17 -0500

    added image column

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_12_07_162446) do
+ActiveRecord::Schema[7.0].define(version: 2022_12_07_162615) do
   create_table "events", force: :cascade do |t|
     t.string "name"
     t.string "venue"
@@ -21,6 +21,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_12_07_162446) do
     t.integer "sg_venue_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "image"
   end
 
   create_table "user_events", force: :cascade do |t|
