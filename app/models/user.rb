fbe01a8bbdd0b58ca89e2b1bec336029a2828a92
Kevin Sasse <sassek70@gmail.com>
2022-12-07 18:23:46 -0500

    added user_event dependency for delete

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
     validates :username, presence: true, uniqueness: true
     validates :password, presence: true
     
-    has_many :user_events
+    has_many :user_events, dependent: :destroy
     has_many :events, through: :user_events
     has_many :user_venues
     has_many :venues, through: :user_venues
