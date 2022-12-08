ca7fe1ec134a519321c7881fe2b8727369dfa64e
Kevin Sasse <sassek70@gmail.com>
2022-12-08 16:24:31 -0500

    updated seeds file

--- app/serializers/event_serializer.rb
+++ app/serializers/event_serializer.rb
@@ -1,3 +1,3 @@
 class EventSerializer < ActiveModel::Serializer
-  attributes :id, :name, :venue, :event_type, :datetime_local, :sg_event_id, :url, :sg_venue_id
+  attributes :id, :name, :venue, :event_type, :datetime_local, :sg_event_id, :url, :sg_venue_id, :image
 end
